3772a80d74021cd298bf6114cfc6bfada9368bb5
6democratickim9 <shcomon24@gmail.com>
2021-03-29 20:12:25 +0900

    0329 ansible

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
 yum install -y epel-release
 yum install -y ansible
+
+mkdir -p /home/vagrant/.vim/autoload /home/vagrant/.vim/bundle
+touch /home/vagrant/.vimrc
+touch /home/vagrant/.bashrc
+
